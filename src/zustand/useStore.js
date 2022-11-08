c37b320c458e23ed9816af28584572bc85f78438
mariorocha2308 <virgoroch852@gmail.com>
2022-11-08 16:38:01 -0600

    add write onchange value of input

--- src/zustand/useStore.js
+++ src/zustand/useStore.js
@@ -12,8 +12,18 @@ const useStore = create((set, get) => ({
   insertItem: (payload) => set((state) => ({ inputs: [...state.inputs, payload]})),
   removeItem: () => set((state) => (state.inputs.pop())),
   writeJson: (id, payload) => {
-    const field = get().inputs.find(input => input._uid === id)
-    field.value = payload
+    console.log(payload);
+    set((state) => {
+      return state.inputs.find(input => {
+        if (input._uid === id) {
+          if (payload === '') {
+            input.value = ''
+            return {...input}
+          }
+          return input.value = payload
+        }
+      })
+    })
   }
 }))
 
